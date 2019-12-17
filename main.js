2b4f1b54591da49c2bdc75cad86eecc8f02dbba5
maxbleed <32711772+maxbleed@users.noreply.github.com>
2019-12-17 17:49:32 +0530

    editted dec 17

---
+++ main.js
@@ -0,0 +1,6 @@
+function getDetails(index) {
+    document.location.href = "details.html";
+    //var game_img = document.getElementById("game_img");
+    console.log("dsaas");
+    //game_img.setAttribute("src","ds3.jpg")
+}
